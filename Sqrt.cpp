34d2aa3f9c7f586bf7d7e97c1c88710c48a09fa5
cuijiaxing <cuijiaxing@gmail.com>
2013-09-21 22:33:19 -0700

    std::sqrt

---
+++ Sqrt.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int sqrt(int x) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        return (int)std::sqrt(x);
+        
+    }
+};
